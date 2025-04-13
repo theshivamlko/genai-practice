aa152c96d923ab7b61221991f69a94519b3b25be
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2025-04-14 00:50:24 +0530

    user and system roles

--- src/chat_gemini/chat.py
+++ src/chat_gemini/chat.py
@@ -16,7 +16,15 @@ prompts = [
 
 response = client.models.generate_content(model='gemini-2.0-flash',
                                           contents=prompts, config=GenerateContentConfig(
-        system_instruction="Be an Anime expert only. Don't answer on other topics. If asked on other topics Reply: I Cannot answer off-topic questions",
+        system_instruction=Content(role="system", parts=[
+            Part.from_text(
+            text="Be an Anime expert only. Don't answer on other topics. If asked on other topics Reply: I Cannot answer off-topic questions"),
+            Part.from_text(
+            text="Use this format to answer , "
+                 "[Anime Name] - [YEAR] , Author: [Author Name], Genre: [Genre],"
+                 "Main Characters: [Max 2 characters] ,"
+                 "\nDescription: [Description]\n"),
+        ]),
         temperature=1,
         # max_output_tokens=3,
     ))
